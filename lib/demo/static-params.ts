efcd1e66fcd6d7197513541a6d3118591e6fefe4
avdeevgreyfog-cmd <avdeevgreyfog@gmail.com>
2026-09-20 22:34:19 +0300

    fix: narrow foundation navigation metadata

--- lib/demo/static-params.ts
+++ lib/demo/static-params.ts
@@ -19,6 +19,6 @@ export const githubPagesStaticParams = {
   ],
   contracts: ["8f000000-0000-4000-8000-000000000001"],
   foundation: flattenNavigation()
-    .filter((item)=>item.status==="foundation")
+    .filter((item)=>"status" in item && item.status==="foundation")
     .map((item)=>item.href.split("?")[0].split("/").filter(Boolean)),
 };
